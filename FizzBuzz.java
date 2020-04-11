93b63714482ef4c9318c41825260016baa3d2367
Omar Diab <42656647+omardballin@users.noreply.github.com>
2020-04-10 19:28:18 -0500

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,15 @@
+for(int i = 0; i < 100; i++){
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%5 == 0){
+                System.out.println("Buzz");
+            }
+            else if(i%3 == 0){
+                System.out.println("Fizz");
+            }
+            else{
+                 System.out.println(i);
+            }
+        }
+        
